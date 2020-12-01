9fedc5e5810964c9dcd707a8c27a428d1df90a7c
Mark Henrick <2178745+markhenrick@users.noreply.github.com>
2020-12-01 20:11:33 +0000

    Day 1-2 Java

--- 2020/java/src/main/java/site/markhenrick/adventofcode2020/Day01.java
+++ 2020/java/src/main/java/site/markhenrick/adventofcode2020/Day01.java
@@ -1,6 +1,9 @@
 package site.markhenrick.adventofcode2020;
 
 import java.util.BitSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 class Day01 {
 	private static final int SUM = 2020;
@@ -16,5 +19,29 @@ class Day01 {
 			}
 		}
 		throw new AssertionError("No solution found");
+	}
+
+	static int part2(final List<Integer> numbers) {
+		final var sumsToProducts = computeTable(numbers);
+		for (final var number : numbers) {
+			final var complement = SUM - number;
+			final var twoProduct = sumsToProducts.get(complement);
+			if (twoProduct != null) {
+				return number * twoProduct;
+			}
+		}
+		throw new AssertionError("No solution found");
+	}
+
+	private static Map<Integer, Integer> computeTable(final List<Integer> numbers) {
+		final Map<Integer, Integer> sumsToProducts = new HashMap<>();
+		for (var i = 0; i < numbers.size(); i++) {
+			final var iVal = numbers.get(i);
+			for (var j = i + 1; j < numbers.size(); j++) {
+				final var jVal = numbers.get(j);
+				sumsToProducts.put(iVal + jVal, iVal * jVal);
+			}
+		}
+		return sumsToProducts;
 	}
 }
