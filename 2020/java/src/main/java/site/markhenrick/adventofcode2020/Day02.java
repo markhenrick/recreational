e0196fa101ff2dce13f18526e7a2a3586cfe69db
Mark Henrick <2178745+markhenrick@users.noreply.github.com>
2020-12-02 19:08:49 +0000

    Day 2-1 Java

---
+++ 2020/java/src/main/java/site/markhenrick/adventofcode2020/Day02.java
@@ -0,0 +1,54 @@
+package site.markhenrick.adventofcode2020;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+class Day02 {
+	private static final Pattern PATTERN = Pattern.compile("(\\d+)-(\\d+) (\\w): (\\w+)");
+
+	static ArrayList<PasswordAndPolicy> parse(final CharSequence input) {
+		final var passwordsAndPolicies = new ArrayList<PasswordAndPolicy>();
+		final var matches = PATTERN.matcher(input);
+		while (matches.find()) {
+			assert matches.groupCount() == 4;
+			final var min = Integer.parseInt(matches.group(1));
+			final var max = Integer.parseInt(matches.group(2));
+			assert min <= max;
+			assert matches.group(3).length() == 1;
+			final var character = matches.group(3).charAt(0);
+			final var password = matches.group(4);
+			passwordsAndPolicies.add(new PasswordAndPolicy(min, max, character, password));
+		}
+		return passwordsAndPolicies;
+	}
+
+	// Arrays.stream doesn't exist for char[], so commented out
+//	static boolean simpleValidatePassword(PasswordAndPolicy passwordAndPolicy) {
+//		final var count = Arrays.stream(passwordAndPolicy.password.toCharArray())
+//			.filter(character -> character == passwordAndPolicy.character)
+//			.count();
+//		return count >= passwordAndPolicy.min && count <= passwordAndPolicy.max;
+//	}
+
+	static boolean fastValidatePassword(final PasswordAndPolicy passwordAndPolicy) {
+		var count = 0;
+		for (final var character : passwordAndPolicy.password.toCharArray()) {
+			if (character == passwordAndPolicy.character) {
+				count++;
+				if (count > passwordAndPolicy.max) {
+					return false;
+				}
+			}
+		}
+		return count >= passwordAndPolicy.min;
+	}
+
+	static long solvePart1(final CharSequence input) {
+		return parse(input).stream()
+			.filter(Day02::fastValidatePassword)
+			.count();
+	}
+
+	static record PasswordAndPolicy(int min, int max, char character, String password) {
+	}
+}
