d30b841fa8105ca7a21823ae1d73b2b4a3c15a4f
Mark Henrick <2178745+markhenrick@users.noreply.github.com>
2020-12-05 14:06:51 +0000

    Day 4-2 Java - misc improvements

--- 2020/java/src/main/java/site/markhenrick/adventofcode2020/Day04.java
+++ 2020/java/src/main/java/site/markhenrick/adventofcode2020/Day04.java
@@ -2,7 +2,6 @@ package site.markhenrick.adventofcode2020;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
@@ -15,16 +14,14 @@ class Day04 {
 	private static final Pattern HAIR_COLOUR = Pattern.compile("^#[0-9a-f]{6}$");
 	private static final Pattern EYE_COLOUR = Pattern.compile("^(amb|blu|brn|gry|grn|hzl|oth)$");
 	private static final Pattern PASSPORT_ID = Pattern.compile("^\\d{9}$");
-	private static final List<String> REQUIRED_FIELDS = List.of("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid");
-	private static final List<Predicate<Map<String, String>>> FULL_REQUIREMENTS = List.of(
-		Day04::validateRecordPart1,
-		record -> isIntegerBetween(record.get("byr"), 1920, 2002),
-		record -> isIntegerBetween(record.get("iyr"), 2010, 2020),
-		record -> isIntegerBetween(record.get("eyr"), 2020, 2030),
-		record -> validateHeight(record.get("hgt")),
-		record -> HAIR_COLOUR.matcher(record.get("hcl")).matches(),
-		record -> EYE_COLOUR.matcher(record.get("ecl")).matches(),
-		record -> PASSPORT_ID.matcher(record.get("pid")).matches()
+	private static final Map<String, Predicate<String>> REQUIREMENTS = Map.of(
+		"byr", isIntegerBetween(1920, 2002),
+		"iyr", isIntegerBetween(2010, 2020),
+		"eyr", isIntegerBetween(2020, 2030),
+		"hgt", Day04::validateHeight,
+		"hcl", HAIR_COLOUR.asMatchPredicate(),
+		"ecl", EYE_COLOUR.asMatchPredicate(),
+		"pid", PASSPORT_ID.asMatchPredicate()
 	);
 
 	static Stream<String> splitRecords(final CharSequence input) {
@@ -42,17 +39,23 @@ class Day04 {
 	}
 
 	static boolean validateRecordPart1(final Map<String, String> record) {
-		return record.keySet().containsAll(REQUIRED_FIELDS);
+		return record.keySet().containsAll(REQUIREMENTS.keySet());
 	}
 
 	static boolean validateRecordPart2(final Map<String, String> record) {
-		return FULL_REQUIREMENTS.stream().allMatch(predicate -> predicate.test(record));
+		return REQUIREMENTS.entrySet().stream()
+			.allMatch(entry -> {
+				final var value = record.get(entry.getKey());
+				return value != null && entry.getValue().test(value);
+			});
 	}
 
-	private static boolean isIntegerBetween(final String input, final int min, final int max) {
-		if (!NUMERIC.matcher(input).matches()) return false;
-		final var number = Integer.parseInt(input);
-		return number >= min && number <= max;
+	private static Predicate<String> isIntegerBetween(final int min, final int max) {
+		return input -> {
+			if (!NUMERIC.matcher(input).matches()) return false;
+			final var number = Integer.parseInt(input);
+			return number >= min && number <= max;
+		};
 	}
 
 	private static boolean validateHeight(final String value) {
@@ -60,11 +63,17 @@ class Day04 {
 		final var unit = value.substring(value.length() - 2);
 		final var number = value.substring(0, value.length() - 2);
 		// isIntegerBetween does a redundant numeric check. Oh well
-		return switch (unit) {
-			case "cm" -> isIntegerBetween(number, 150, 193);
-			case "in" -> isIntegerBetween(number, 59, 76);
-			default -> false;
+		final Predicate<String> predicate = switch (unit) {
+			case "cm" -> isIntegerBetween(150, 193);
+			case "in" -> isIntegerBetween(59, 76);
+			default -> constantPredicate(false);
 		};
+		return predicate.test(number);
+	}
+
+	// Probably in the stdlib somewhere
+	private static <A> Predicate<A> constantPredicate(final boolean value) {
+		return a -> value;
 	}
 
 	private static long countRecordsSatisfying(final Predicate<? super Map<String, String>> predicate, final CharSequence input) {
