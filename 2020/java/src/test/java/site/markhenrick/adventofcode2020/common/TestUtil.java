cb0d88f8b805b9f83a32fa6071a6bb5f594305c8
Mark Henrick <2178745+markhenrick@users.noreply.github.com>
2020-12-06 18:20:18 +0000

    Fix #2 by converting newlines on test file read

--- 2020/java/src/test/java/site/markhenrick/adventofcode2020/common/TestUtil.java
+++ 2020/java/src/test/java/site/markhenrick/adventofcode2020/common/TestUtil.java
@@ -4,17 +4,25 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.regex.Pattern;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestUtil {
+
+	private static final Pattern WINDOWS_NEWLINE = Pattern.compile("\r\n");
+
 	public static String getResourceAsString(final String filename) {
 		final var resource = TestUtil.class.getClassLoader().getResource(filename);
 		assertThat(resource).isNotNull();
 		try {
-			return Files.readString(Paths.get(resource.toURI()));
+			return dos2unix(Files.readString(Paths.get(resource.toURI())));
 		} catch (IOException | URISyntaxException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	public static String dos2unix(final CharSequence input) {
+		return WINDOWS_NEWLINE.matcher(input).replaceAll("\n");
 	}
 }
