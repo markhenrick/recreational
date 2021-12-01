ee59ae30166ee7fe187be5d0febaf37472c8721b
Mark Henrick <2178745+markhenrick@users.noreply.github.com>
2021-12-01 22:50:58 +0000

    2021d1p2 Haskell

--- haskell/y2021/day01.hs
+++ haskell/y2021/day01.hs
@@ -1,4 +1,10 @@
-parse rawInput = map read (words rawInput) :: [Int]
 day01p1 input = (length . filter id) $ zipWith (<) input (tail input)
+zipPlus = zipWith (+)
+intoWindows input = zipPlus (zipPlus input (tail input)) (tail (tail input))
+day01p2 = day01p1 . intoWindows
 
-main = interact (show . day01p1 . parse)
+main = do
+    rawInput <- getContents
+    let input = map read (words rawInput) :: [Int]
+    print (day01p1 input)
+    print (day01p2 input)
