31ec5a7152427e2d248e2608e15bde17975fe109
Mark Henrick <2178745+markhenrick@users.noreply.github.com>
2021-12-06 18:32:05 +0000

    2021d6 Java

--- java/src/main/java/site/markhenrick/adventofcode/common/StringUtil.java
+++ java/src/main/java/site/markhenrick/adventofcode/common/StringUtil.java
@@ -10,6 +10,7 @@ public class StringUtil {
 	public static final Function<String, Stream<String>> LINE_SPLITTER = splitToStream("\n");
 	public static final Function<String, Stream<String>> RECORD_SPLITTER = splitToStream("\n\n");
 	public static final Function<String, Stream<String>> WORD_SPLITTER = splitToStream(" ");
+	public static final Function<String, Stream<String>> COMMA_SPLITTER = splitToStream(",");
 
 	public static Function<String, Stream<String>> splitToStream(final String regex) {
 		final var pattern = Pattern.compile(regex);
