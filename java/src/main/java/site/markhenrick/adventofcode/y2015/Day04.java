4649fe8b935e9a24f557afe3c65bfd688f1cf459
Mark Henrick <2178745+markhenrick@users.noreply.github.com>
2021-12-06 20:48:06 +0000

    2015d4 Java - rewrite

--- java/src/main/java/site/markhenrick/adventofcode/y2015/Day04.java
+++ java/src/main/java/site/markhenrick/adventofcode/y2015/Day04.java
@@ -4,21 +4,23 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.function.Predicate;
+import java.util.stream.IntStream;
 
 public class Day04 {
 	static int search(String key, Predicate<byte[]> predicate) throws NoSuchAlgorithmException {
 		var digest = MessageDigest.getInstance("MD5");
-		for (int i = 0; true; i++) {
-			var string = key + i;
-			var hash = digest.digest(string.getBytes(StandardCharsets.UTF_8));
-			if (predicate.test(hash)) {
-				return i;
-			}
-		}
+		return IntStream.rangeClosed(0, Integer.MAX_VALUE)
+			.filter(i -> {
+				var string = key + i;
+				var hash = digest.digest(string.getBytes(StandardCharsets.UTF_8));
+				return predicate.test(hash);
+			})
+			.findFirst()
+			.orElseThrow();
 	}
 
 	static int part1(String key) throws NoSuchAlgorithmException {
-		return search(key, hash -> hash[0] == 0x00 && hash[1] == 0x00 && hash[2] > 0 && hash[2] < 0x10);
+		return search(key, hash -> hash[0] == 0x00 && hash[1] == 0x00 && hash[2] > 0x00 && hash[2] < 0x10);
 	}
 
 	static int part2(String key) throws NoSuchAlgorithmException {
