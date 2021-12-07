b2b087376159cfa046dced0412e1bdca52bfa77c
Mark Henrick <2178745+markhenrick@users.noreply.github.com>
2021-12-07 17:08:34 +0000

    2021d7p1 Java - unstupidify

--- java/src/main/java/site/markhenrick/adventofcode/y2021/Day07.java
+++ java/src/main/java/site/markhenrick/adventofcode/y2021/Day07.java
@@ -2,34 +2,35 @@ package site.markhenrick.adventofcode.y2021;
 
 import site.markhenrick.adventofcode.common.StringUtil;
 
-import java.util.List;
+import java.util.Arrays;
 import java.util.function.IntBinaryOperator;
 import java.util.stream.IntStream;
 
 public class Day07 {
-	static List<Integer> parse(String input) {
+	static int[] parse(String input) {
 		return StringUtil.COMMA_SPLITTER.apply(input.trim())
-				.map(Integer::parseInt)
-				.toList();
+				.mapToInt(Integer::parseInt)
+				.toArray();
 	}
 
-	static int solve(List<Integer> input, IntBinaryOperator crabCost) {
-		var max = input.stream().max(Integer::compare).orElseThrow();
+	static int exhaustiveSearch(int[] input, IntBinaryOperator crabCost) {
+		var max = Arrays.stream(input).max().orElseThrow();
 		return IntStream.range(0, max + 1)
-				.map(p -> input.stream()
-						.mapToInt(x -> crabCost.applyAsInt(p, x))
+				.map(p -> Arrays.stream(input)
+						.map(x -> crabCost.applyAsInt(p, x))
 						.sum()
 				)
 				.min()
 				.orElseThrow();
 	}
 
-	static int part1(List<Integer> input) {
-		return solve(input, Day07::distance);
+	static int part1(int[] input) {
+		var optimalPosition = median(input);
+		return Arrays.stream(input).map(x -> Math.abs(x - optimalPosition)).sum();
 	}
 
-	static int part2(List<Integer> input) {
-		return solve(input, (p, x) -> intSummation(distance(p, x)));
+	static int part2(int[] input) {
+		return exhaustiveSearch(input, (p, x) -> intSummation(distance(p, x)));
 	}
 
 	static int distance(int a, int b) {
@@ -38,5 +39,53 @@ public class Day07 {
 
 	static int intSummation(int limitInclusive) {
 		return limitInclusive * (limitInclusive + 1) / 2;
+	}
+
+	static int median(int[] numbers) {
+		int[] destructibleNumbers = new int[numbers.length];
+		System.arraycopy(numbers, 0, destructibleNumbers, 0, numbers.length);
+		return quickSelect(destructibleNumbers, destructibleNumbers.length / 2);
+	}
+
+	// Thank you, Wikipedia
+	static int quickSelect(int[] numbers, int k) {
+		int start = 0;
+		int end = numbers.length - 1;
+		while (true) {
+			assert start >= 0 && end < numbers.length && start <= end;
+			if (start == end) {
+				return numbers[start];
+			}
+			int pivotIndex = (start + end) / 2;
+			assert pivotIndex >= start && pivotIndex <= end;
+			pivotIndex = quickPartition(numbers, start, end, pivotIndex);
+			if (k == pivotIndex) {
+				return numbers[k];
+			} else if (k < pivotIndex) {
+				end -= 1;
+			} else {
+				start += 1;
+			}
+		}
+	}
+
+	static int quickPartition(int[] numbers, int start, int end, int pivotIndex) {
+		int pivotVal = numbers[pivotIndex];
+		swap(numbers, pivotIndex, end);
+		int newIndex = start;
+		for (int i = start; i < end; i++) {
+			if (numbers[i] < pivotVal) {
+				swap(numbers, newIndex, i);
+				newIndex++;
+			}
+		}
+		swap(numbers, end, newIndex);
+		return newIndex;
+	}
+
+	static void swap(int[] arr, int i, int j) {
+		int tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
 	}
 }
