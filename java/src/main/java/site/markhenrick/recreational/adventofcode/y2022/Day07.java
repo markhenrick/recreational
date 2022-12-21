d4ac70af0a2e9c7a59089bc2ee615ac117c75c56
Mark Henrick <2178745+markhenrick@users.noreply.github.com>
2022-12-21 22:59:42 +0000

    2022d7p1 Java - to be improved

--- java/src/main/java/site/markhenrick/recreational/adventofcode/y2022/Day07.java
+++ java/src/main/java/site/markhenrick/recreational/adventofcode/y2022/Day07.java
@@ -2,14 +2,31 @@ package site.markhenrick.recreational.adventofcode.y2022;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import site.markhenrick.recreational.common.StringUtil;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day07 {
     private static final long SIZE_THRESHOLD = 100000;
+    private static final String ROOT_STRING = "/";
+    private static final String UP_STRING = "..";
+    private static final Pattern PATTERN_CD = Pattern.compile("^\\$ cd (.*)$");
+    private static final Pattern PATTERN_LS = Pattern.compile("^\\$ ls$");
+    private static final Pattern PATTERN_DIR = Pattern.compile("^dir (.*)$");
+    private static final Pattern PATTERN_FILE = Pattern.compile("^(\\d*) (.*)$");
+
+    public static long part1(String input) {
+        var tree = Directory.parse(input);
+        return tree.countSmallDirs();
+    }
 
     @Data
+    @EqualsAndHashCode(exclude = "parent")
     public abstract static class Node {
+        private Directory parent;
 
         public abstract long getSize();
 
@@ -43,14 +60,60 @@ public class Day07 {
     @EqualsAndHashCode(callSuper = true)
     @Data
     public static class Directory extends Node {
-        private Map<String, Node> children;
+        private Map<String, Node> children = new HashMap<>();
+
+        public static Directory parse(String input) {
+            Directory root = new Directory();
+            Directory cwd = root;
+            var lines = StringUtil.LINE_SPLITTER.apply(input).toList();
+            for (var line : lines) {
+                Matcher matchCd;
+                Matcher matchLs;
+                Matcher matchDir;
+                Matcher matchFile;
+                if ((matchCd = PATTERN_CD.matcher(line)).matches()) {
+                    assert matchCd.groupCount() == 1;
+                    var directory = matchCd.group(1);
+                    if (directory.equals(ROOT_STRING)) {
+                        cwd = root;
+                    } else if (directory.equals(UP_STRING)) {
+                        cwd = cwd.getParent();
+                    } else {
+                        // We'll let the ClassCastException be our validation
+                        cwd = (Directory) cwd.getChild(directory);
+                    }
+                } else if ((matchLs = PATTERN_LS.matcher(line)).matches()) {
+                    // Nothing to do actually
+                } else if ((matchDir = PATTERN_DIR.matcher(line)).matches()) {
+                    assert matchDir.groupCount() == 1;
+                    cwd.addChild(matchDir.group(1), new Directory());
+                } else if ((matchFile = PATTERN_FILE.matcher(line)).matches()) {
+                    assert matchFile.groupCount() == 2;
+                    cwd.addChild(matchFile.group(2), new File(Long.parseLong(matchFile.group(1))));
+                } else {
+                    throw new AssertionError("Unrecognised line: " + line);
+                }
+            }
+            return root;
+        }
 
         public Directory() {
 
         }
 
         public Directory(Map<String, Node> children) {
-            this.setChildren(children);
+            for (var entry : children.entrySet()) {
+                addChild(entry.getKey(), entry.getValue());
+            }
+        }
+
+        public void addChild(String name, Node child) {
+            child.setParent(this);
+            children.put(name, child);
+        }
+
+        public Node getChild(String name) {
+            return children.get(name);
         }
 
         @Override
--- java/src/test/java/site/markhenrick/recreational/adventofcode/y2022/Day07Test.java
+++ java/src/test/java/site/markhenrick/recreational/adventofcode/y2022/Day07Test.java
@@ -1,13 +1,45 @@
 package site.markhenrick.recreational.adventofcode.y2022;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import site.markhenrick.recreational.common.TestUtil;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class Day07Test {
+    private static final String SAMPLE_INPUT = """
+$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+""";
+
     private static final Day07.Node SAMPLE_TREE = new Day07.Directory(Map.of(
             "a", new Day07.Directory(Map.of(
                     "e", new Day07.Directory(Map.of(
@@ -25,7 +57,7 @@ public class Day07Test {
                     "d.ext", new Day07.File(5626152),
                     "k", new Day07.File(7214296)
             ))
-    ) );
+    ));
 
     @Test
     void getSize() {
@@ -37,5 +69,24 @@ public class Day07Test {
     @Test
     void countSmallDirs() {
         assertThat(SAMPLE_TREE.countSmallDirs()).isEqualTo(95437L);
+    }
+
+    @Test
+    void parse() {
+        var result = Day07.Directory.parse(SAMPLE_INPUT);
+        assertThat(result).isEqualTo(SAMPLE_TREE);
+    }
+
+    static Stream<Arguments> part1() {
+        return Stream.of(
+                arguments(SAMPLE_INPUT, 95437L),
+                arguments(TestUtil.getResourceAsString("AoC/input/2022/day07.txt"), 1182909L)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void part1(String input, long expected) {
+        assertThat(Day07.part1(input)).isEqualTo(expected);
     }
 }
