d7b0fc632c65a420d1e0f3b0ad439c7adceff5bb
Mark Henrick <2178745+markhenrick@users.noreply.github.com>
2023-12-01 13:05:08 +0000

    2023d1p2 Java. Passes tests and sample but fails actual

--- java/src/main/java/site/markhenrick/recreational/adventofcode/y2023/Day01.java
+++ java/src/main/java/site/markhenrick/recreational/adventofcode/y2023/Day01.java
@@ -1,7 +1,15 @@
 package site.markhenrick.recreational.adventofcode.y2023;
 
 import lombok.val;
+import org.javatuples.Pair;
 import site.markhenrick.recreational.common.StringUtil;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import static site.markhenrick.recreational.common.StringUtil.LINE_SPLITTER;
 
@@ -9,19 +17,67 @@ public class Day01 {
 	// Decided to go with a nice streams approach rather than a faster but ugly state machine
 	// time and space should only differ by a constant factor :)
 
+	private static final Pattern PART2_PATTERN = Pattern.compile("(\\d|one|two|three|four|five|six|seven|eight|nine)");
+	//<editor-fold desc="zero -> 0 etc." defaultstate="collapsed">
+	private static final Map<String, Integer> DIGIT_MAP = Map.ofEntries(
+			// The only part of this codebase where I used ChatGPT. Not typing all this myself lol
+			Map.entry("0", 0),
+			Map.entry("zero", 0),
+			Map.entry("1", 1),
+			Map.entry("one", 1),
+			Map.entry("2", 2),
+			Map.entry("two", 2),
+			Map.entry("3", 3),
+			Map.entry("three", 3),
+			Map.entry("4", 4),
+			Map.entry("four", 4),
+			Map.entry("5", 5),
+			Map.entry("five", 5),
+			Map.entry("6", 6),
+			Map.entry("six", 6),
+			Map.entry("7", 7),
+			Map.entry("seven", 7),
+			Map.entry("8", 8),
+			Map.entry("eight", 8),
+			Map.entry("9", 9),
+			Map.entry("nine", 9)
+	);
+	//</editor-fold>
+
 	public static int part1(String input) {
+		return processFile(input, Day01::p1Splitter);
+	}
+
+	public static int part2(String input) {
+		return processFile(input, Day01::p2Splitter);
+	}
+
+	private static int processFile(String input, Function<String, Stream<Integer>> splitter) {
 		return LINE_SPLITTER.apply(input)
-				.mapToInt(Day01::processOneLine)
+				.mapToInt(processLineWith(splitter))
 				.sum();
 	}
 
-	static int processOneLine(String line) {
+	static ToIntFunction<String> processLineWith(Function<String, Stream<Integer>> splitter) {
+		return line -> {
+			val digits = firstAndLast(splitter.apply(line)
+					.filter(digit -> digit >= 0 && digit <= 9)
+					.toList());
+			return 10 * digits.getValue0() + digits.getValue1();
+		};
+	}
+
+	static Stream<Integer> p1Splitter(String line) {
+		return StringUtil.charStream(line).map(character -> character - '0');
+	}
+
+	static Stream<Integer> p2Splitter(String line) {
+		return PART2_PATTERN.matcher(line).results().map(result -> DIGIT_MAP.get(result.group(1)));
+	}
+
+	private static <T> Pair<T, T> firstAndLast(List<T> list) {
 		// TODO custom collector so I'm not allocating a whole list just to get [0] and [-1]
-		val digits = StringUtil.charStream(line)
-				.map(character -> character - '0')
-				.filter(digit -> digit >= 0 && digit <= 9)
-				.toList();
-		assert !digits.isEmpty();
-		return 10 * digits.get(0) + digits.get(digits.size() - 1);
+		assert !list.isEmpty();
+		return Pair.with(list.get(0), list.get(list.size() - 1));
 	}
 }
--- java/src/test/java/site/markhenrick/recreational/adventofcode/y2023/Day01Test.java
+++ java/src/test/java/site/markhenrick/recreational/adventofcode/y2023/Day01Test.java
@@ -12,16 +12,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class Day01Test {
-	private static final String SAMPLE_INPUT = """
+	private static final String SAMPLE_INPUT_P1 = """
 1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet
 """;
 
+	private static final String SAMPLE_INPUT_P2 = """
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+""";
+
 	static Stream<Arguments> part1() {
 		return Stream.of(
-				arguments(142, SAMPLE_INPUT),
+				arguments(142, SAMPLE_INPUT_P1),
 				arguments(55130, TestUtil.getResourceAsString("AoC/input/2023/day01.txt"))
 		);
 	}
@@ -33,6 +43,21 @@ treb7uchet
 		assertThat(Day01.part1(input)).isEqualTo(expected);
 	}
 
+	static Stream<Arguments> part2() {
+		return Stream.of(
+				arguments(142, SAMPLE_INPUT_P1),
+				arguments(281, SAMPLE_INPUT_P2),
+				arguments(55130, TestUtil.getResourceAsString("AoC/input/2023/day01.txt"))
+		);
+	}
+
+	@ParameterizedTest
+	@MethodSource
+	void part2(int expected, String input)
+	{
+		assertThat(Day01.part2(input)).isEqualTo(expected);
+	}
+
 	@ParameterizedTest
 	@CsvSource({
 			"1abc2,12",
@@ -40,7 +65,25 @@ treb7uchet
 			"a1b2c3d4e5f,15",
 			"treb7uchet,77",
 	})
-	void processOneLine(String line, int expected) {
-		assertThat(Day01.processOneLine(line)).isEqualTo(expected);
+	void processLineP1(String line, int expected) {
+		assertThat(Day01.processLineWith(Day01::p1Splitter).applyAsInt(line)).isEqualTo(expected);
+	}
+
+	@ParameterizedTest
+	@CsvSource({
+			"1abc2,12",
+			"pqr3stu8vwx,38",
+			"a1b2c3d4e5f,15",
+			"treb7uchet,77",
+			"two1nine,29",
+			"eightwothree,83",
+			"abcone2threexyz,13",
+			"xtwone3four,24",
+			"4nineeightseven2,42",
+			"zoneight234,14",
+			"7pqrstsixteen,76"
+	})
+	void processLineP2(String line, int expected) {
+		assertThat(Day01.processLineWith(Day01::p2Splitter).applyAsInt(line)).isEqualTo(expected);
 	}
 }
