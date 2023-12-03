3743cae8da45371bdde557d9f32fa0ad17d5cb0f
Mark Henrick <2178745+markhenrick@users.noreply.github.com>
2023-12-03 04:38:56 +0000

    2023d2 Java. More IntVec3 stuff

--- java/src/main/java/site/markhenrick/recreational/common/data/IntVec3.java
+++ java/src/main/java/site/markhenrick/recreational/common/data/IntVec3.java
@@ -1,6 +1,9 @@
 package site.markhenrick.recreational.common.data;
 
+import java.util.function.BiFunction;
 import java.util.function.IntBinaryOperator;
+import java.util.function.IntPredicate;
+import java.util.function.IntUnaryOperator;
 
 public record IntVec3(int x, int y, int z) {
 	// TODO perhaps an IntVec wrapping an arbitrarily sized int[] is called for. Just assert matching size
@@ -9,16 +12,29 @@ public record IntVec3(int x, int y, int z) {
 	public static final IntVec3 UNIT_Y = new IntVec3(0, 1, 0);
 	public static final IntVec3 UNIT_Z = new IntVec3(0, 0, 1);
 
-	public IntVec3 add(IntVec3 addendum) {
-		return new IntVec3(this.x + addendum.x, this.y + addendum.y, this.z + addendum.z);
-	}
-
-	public IntVec3 scale(int scalar) {
-		return new IntVec3(x * scalar, y * scalar, z * scalar);
+	public IntVec3 map(IntUnaryOperator fn) {
+		return new IntVec3(fn.applyAsInt(x), fn.applyAsInt(y), fn.applyAsInt(z));
 	}
 
 	public IntVec3 reduce(IntBinaryOperator fn, IntVec3 other) {
+		//noinspection SuspiciousNameCombination
 		return new IntVec3(fn.applyAsInt(this.x, other.x), fn.applyAsInt(this.y, other.y), fn.applyAsInt(this.z, other.z));
+	}
+
+	public boolean allMatch(IntPredicate pred) {
+		return pred.test(x) && pred.test(y) && pred.test(z);
+	}
+
+	public boolean anyMatch(IntPredicate pred) {
+		return pred.test(x) || pred.test(y) || pred.test(z);
+	}
+
+	public boolean allMatch(BiFunction<Integer, Integer, Boolean> fn, IntVec3 other) {
+		return fn.apply(this.x, other.x) && fn.apply(this.y, other.y) && fn.apply(this.z, other.z);
+	}
+
+	public boolean anyMatch(BiFunction<Integer, Integer, Boolean> fn, IntVec3 other) {
+		return fn.apply(this.x, other.x) || fn.apply(this.y, other.y) || fn.apply(this.z, other.z);
 	}
 
 	public int componentSum() {
@@ -29,11 +45,19 @@ public record IntVec3(int x, int y, int z) {
 		return x * y * z;
 	}
 
+	public IntVec3 add(IntVec3 addendum) {
+		return reduce(Integer::sum, addendum);
+	}
+
+	public IntVec3 scale(int scalar) {
+		return map(x -> x * scalar);
+	}
+
 	public boolean boundedBy(IntVec3 other) {
-		return this.x <= other.x && this.y <= other.y && this.z <= other.z;
+		return allMatch((x, y) -> x <= y, other);
 	}
 
 	public boolean strictlyBoundedBy(IntVec3 other) {
-		return this.x < other.x && this.y < other.y && this.z < other.z;
+		return allMatch((x, y) -> x < y, other);
 	}
 }
