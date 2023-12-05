a39cde6d25bb0852d991e983bc072148f993ee24
Mark Henrick <2178745+markhenrick@users.noreply.github.com>
2023-12-05 12:48:49 +0000

    Revert "2023d5p2 Java. Remove unused name parsing"
    This reverts commit 40bfc441b765dadafd4d08b10bc53ce4c5feddf0.

--- java/src/main/java/site/markhenrick/recreational/adventofcode/y2023/Day05.java
+++ java/src/main/java/site/markhenrick/recreational/adventofcode/y2023/Day05.java
@@ -6,11 +6,13 @@ import site.markhenrick.recreational.common.FunctionalUtil.Pair;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import static site.markhenrick.recreational.common.StringUtil.WORD_SPLITTER;
 
 public class Day05 {
 	private static final String SEEDS_PREFIX = "seeds: ";
+	private static final Pattern SET_NAMES_PATTERN = Pattern.compile("^([a-z]*)-to-([a-z]*) map:$");
 
 	public static long part1(String input) {
 		val parsed = parseInputP1(input);
@@ -22,6 +24,7 @@ public class Day05 {
 				.getAsLong();
 	}
 
+	// TODO more clever solution in general
 	public static long part2(String input) {
 		val parsed = parseInputP2(input);
 		val ranges = parsed.l();
@@ -40,7 +43,7 @@ public class Day05 {
 	// TODO compose all the functions *first*, then apply
 
 	// TODO this is just iterate() right?
-	static long applyFns(List<List<FunctionPiece>> fns, long input) {
+	static long applyFns(List<PiecewiseFunction> fns, long input) {
 		var current = input;
 		for (val fn : fns) {
 			current = applyFn(fn, current);
@@ -49,9 +52,9 @@ public class Day05 {
 	}
 
 	// TODO this should be a method of PiecewiseFunction
-	static long applyFn(List<FunctionPiece> fn, long input) {
+	static long applyFn(PiecewiseFunction fn, long input) {
 		// Imperative for now. May convert to streams later, performance permitting
-		for (val piece : fn) {
+		for (val piece : fn.pieces) {
 			val offset = input - piece.srcStart;
 			if (offset >= 0 && offset < piece.length) {
 				return piece.destStart + offset;
@@ -64,7 +67,7 @@ public class Day05 {
 	 * Parsing code
 	 */
 
-	static Pair<List<Long>, List<List<FunctionPiece>>> parseInputP1(String input) {
+	static Pair<List<Long>, List<PiecewiseFunction>> parseInputP1(String input) {
 		val records = input.split("\n\n");
 		assert records.length > 1;
 		assert records[0].startsWith(SEEDS_PREFIX);
@@ -78,7 +81,7 @@ public class Day05 {
 		return new Pair<>(seeds, functions);
 	}
 
-	static Pair<List<SeedRange>, List<List<FunctionPiece>>> parseInputP2(String input) {
+	static Pair<List<SeedRange>, List<PiecewiseFunction>> parseInputP2(String input) {
 		// TODO merge duplicate code with above
 		val records = input.split("\n\n");
 		assert records.length > 1;
@@ -101,12 +104,26 @@ public class Day05 {
 		return list;
 	}
 
-	static List<FunctionPiece> parseFunction(String input) {
+	static PiecewiseFunction parseFunction(String input) {
 		val lines = input.split("\n");
 		assert lines.length > 1;
-		return Arrays.stream(lines, 1, lines.length)
+		val setNames = parseSetNames(lines[0]);
+		val pieces = Arrays.stream(lines, 1, lines.length)
 				.map(Day05::parseFunctionPiece)
 				.toList();
+		return new PiecewiseFunction(setNames.l(), setNames.r(), pieces);
+	}
+
+	private static Pair<String, String> parseSetNames(String input) {
+		val matcher = SET_NAMES_PATTERN.matcher(input);
+		var matched = matcher.find();
+		assert matched;
+		assert matcher.groupCount() == 2;
+		val srcName = matcher.group(1);
+		val destName = matcher.group(2);
+		matched = matcher.find();
+		assert !matched;
+		return new Pair<>(srcName, destName);
 	}
 
 	private static FunctionPiece parseFunctionPiece(String input) {
@@ -116,5 +133,7 @@ public class Day05 {
 	}
 
 	record SeedRange(long start, long length) {}
+	// TODO I'm not gonna need these names am I
+	record PiecewiseFunction(String srcName, String destName, List<FunctionPiece> pieces) {}
 	record FunctionPiece(long destStart, long srcStart, long length) {}
 }
