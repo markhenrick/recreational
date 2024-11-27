164d27d7cdba80dad046531d2090f7e39b6b0cbd
Mark Henrick <2178745+markhenrick@users.noreply.github.com>
2024-11-27 21:21:48 +0000

    2016d1p1 Kotlin - Stupid solution

--- kotlin/src/main/kotlin/site/markhenrick/recreational/adventofcode/y2016/Day01.kt
+++ kotlin/src/main/kotlin/site/markhenrick/recreational/adventofcode/y2016/Day01.kt
@@ -13,6 +13,16 @@ private val COMPASS = listOf(UNIT_Y, UNIT_X, -UNIT_Y, -UNIT_X)
 // TODO oh dear this is in the top-level package namespace. Will have to decide what to do about that before day 2
 fun solvePart1(input: String): Int = walk(input).last().metricSize()
 
+fun solvePart2(input: String): Int {
+    val seen = mutableSetOf<IntVec2>()
+    for (location in walk(input)) {
+        if (!seen.add(location)) {
+            return location.metricSize()
+        }
+    }
+    throw AssertionError("No retracted step")
+}
+
 // TODO consider parsing the input up front
 
 fun walk(input: String): Sequence<IntVec2> = sequence {
@@ -27,7 +37,10 @@ fun walk(input: String): Sequence<IntVec2> = sequence {
             headingIndex = (headingIndex + 1 ) wrapMod COMPASS.size
         }
         val stepCount = instruction.substring(1).toInt()
-        location += COMPASS[headingIndex] * stepCount
-        yield(location)
+        // TODO this is a really stupid/inefficient solution. Find the line intersections instead
+        for (i in 1..stepCount) {
+            location += COMPASS[headingIndex]
+            yield(location)
+        }
     }
 }
--- kotlin/src/test/kotlin/site/markhenrick/recreational/adventofcode/y2016/Day01Test.kt
+++ kotlin/src/test/kotlin/site/markhenrick/recreational/adventofcode/y2016/Day01Test.kt
@@ -1,6 +1,6 @@
 package site.markhenrick.recreational.adventofcode.y2016
 
-import org.assertj.core.api.Java6Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -8,11 +8,20 @@ import site.markhenrick.recreational.common.IntVec2
 import site.markhenrick.recreational.common.ORIGIN
 import java.util.stream.Stream
 
+// TODO set up file reading for day 2
+private const val ACTUAL_INPUT = "L4, L3, R1, L4, R2, R2, L1, L2, R1, R1, L3, R5, L2, R5, L4, L3, R2, R2, L5, L1, R4, L1, R3, L3, R5, R2, L5, R2, R1, R1, L5, R1, L3, L2, L5, R4, R4, L2, L1, L1, R1, R1, L185, R4, L1, L1, R5, R1, L1, L3, L2, L1, R2, R2, R2, L1, L1, R4, R5, R53, L1, R1, R78, R3, R4, L1, R5, L1, L4, R3, R3, L3, L3, R191, R4, R1, L4, L1, R3, L1, L2, R3, R2, R4, R5, R5, L3, L5, R2, R3, L1, L1, L3, R1, R4, R1, R3, R4, R4, R4, R5, R2, L5, R1, R2, R5, L3, L4, R1, L5, R1, L4, L3, R5, R5, L3, L4, L4, R2, R2, L5, R3, R1, R2, R5, L5, L3, R4, L5, R5, L3, R1, L1, R4, R4, L3, R2, R5, R1, R2, L1, R4, R1, L3, L3, L5, R2, R5, L1, L4, R3, R3, L3, R2, L5, R1, R3, L3, R2, L1, R4, R3, L4, R5, L2, L2, R5, R1, R2, L4, L4, L5, R3, L4"
+
 class Day01Test {
     @ParameterizedTest
     @MethodSource
     fun part1(input: String, expected: Int) {
         assertThat(solvePart1(input)).isEqualTo(expected)
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    fun part2(input: String, expected: Int) {
+        assertThat(solvePart2(input)).isEqualTo(expected)
     }
 
     @ParameterizedTest
@@ -27,8 +36,13 @@ class Day01Test {
             Arguments.of("R2, L3", 5),
             Arguments.of("R2, R2, R2", 2),
             Arguments.of("R5, L5, R5, R3", 12),
-            // TODO set up file reading for day 2
-            Arguments.of("L4, L3, R1, L4, R2, R2, L1, L2, R1, R1, L3, R5, L2, R5, L4, L3, R2, R2, L5, L1, R4, L1, R3, L3, R5, R2, L5, R2, R1, R1, L5, R1, L3, L2, L5, R4, R4, L2, L1, L1, R1, R1, L185, R4, L1, L1, R5, R1, L1, L3, L2, L1, R2, R2, R2, L1, L1, R4, R5, R53, L1, R1, R78, R3, R4, L1, R5, L1, L4, R3, R3, L3, L3, R191, R4, R1, L4, L1, R3, L1, L2, R3, R2, R4, R5, R5, L3, L5, R2, R3, L1, L1, L3, R1, R4, R1, R3, R4, R4, R4, R5, R2, L5, R1, R2, R5, L3, L4, R1, L5, R1, L4, L3, R5, R5, L3, L4, L4, R2, R2, L5, R3, R1, R2, R5, L5, L3, R4, L5, R5, L3, R1, L1, R4, R4, L3, R2, R5, R1, R2, L1, R4, R1, L3, L3, L5, R2, R5, L1, L4, R3, R3, L3, R2, L5, R1, R3, L3, R2, L1, R4, R3, L4, R5, L2, L2, R5, R1, R2, L4, L4, L5, R3, L4", 332),
+            Arguments.of(ACTUAL_INPUT, 332),
+        )
+
+        @JvmStatic
+        fun part2(): Stream<Arguments> = Stream.of(
+            Arguments.of("R8, R4, R4, R8", 4),
+            Arguments.of(ACTUAL_INPUT, 166),
         )
 
         @JvmStatic
@@ -37,7 +51,10 @@ class Day01Test {
                 "R2, L3",
                 listOf(
                     ORIGIN,
+                    IntVec2(1, 0),
                     IntVec2(2, 0),
+                    IntVec2(2, 1),
+                    IntVec2(2, 2),
                     IntVec2(2, 3),
                 )
             ),
@@ -45,8 +62,11 @@ class Day01Test {
                 "R2, R2, R2",
                 listOf(
                     ORIGIN,
+                    IntVec2(1, 0),
                     IntVec2(2, 0),
+                    IntVec2(2, -1),
                     IntVec2(2, -2),
+                    IntVec2(1, -2),
                     IntVec2(0, -2),
                 )
             ),
@@ -54,12 +74,56 @@ class Day01Test {
                 "R5, L5, R5, R3",
                 listOf(
                     ORIGIN,
+                    IntVec2(1, 0),
+                    IntVec2(2, 0),
+                    IntVec2(3, 0),
+                    IntVec2(4, 0),
                     IntVec2(5, 0),
+                    IntVec2(5, 1),
+                    IntVec2(5, 2),
+                    IntVec2(5, 3),
+                    IntVec2(5, 4),
                     IntVec2(5, 5),
+                    IntVec2(6, 5),
+                    IntVec2(7, 5),
+                    IntVec2(8, 5),
+                    IntVec2(9, 5),
                     IntVec2(10, 5),
+                    IntVec2(10, 4),
+                    IntVec2(10, 3),
                     IntVec2(10, 2),
                 )
             ),
+            Arguments.of(
+                "R8, R4, R4, R8",
+                listOf(
+                    ORIGIN,
+                    IntVec2(1, 0),
+                    IntVec2(2, 0),
+                    IntVec2(3, 0),
+                    IntVec2(4, 0),
+                    IntVec2(5, 0),
+                    IntVec2(6, 0),
+                    IntVec2(7, 0),
+                    IntVec2(8, 0),
+                    IntVec2(8, -1),
+                    IntVec2(8, -2),
+                    IntVec2(8, -3),
+                    IntVec2(8, -4),
+                    IntVec2(7, -4),
+                    IntVec2(6, -4),
+                    IntVec2(5, -4),
+                    IntVec2(4, -4),
+                    IntVec2(4, -3),
+                    IntVec2(4, -2),
+                    IntVec2(4, -1),
+                    IntVec2(4, 0),
+                    IntVec2(4, 1),
+                    IntVec2(4, 2),
+                    IntVec2(4, 3),
+                    IntVec2(4, 4),
+                )
+            )
         )
     }
 }
