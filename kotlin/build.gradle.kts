c1784af32c72290128076e83aae82feb19df081c
Mark Henrick <2178745+markhenrick@users.noreply.github.com>
2024-11-27 19:24:53 +0000

    Fix JDK 23 build and add deps

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
-    kotlin("jvm") version "2.0.21"
+    kotlin("jvm") version "2.1.0"
+}
+
+kotlin {
+    jvmToolchain(23)
 }
 
 group = "site.markhenrick.recreational"
@@ -10,7 +14,13 @@ repositories {
 }
 
 dependencies {
+    // TODO see if we still need all these at the end
+    implementation("org.javatuples:javatuples:1.2")
+    implementation("com.codepoetics:protonpack:1.16")
     testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.3")
+    testImplementation("org.assertj:assertj-core:3.26.3")
+    // TODO consider moving to Kotest in the future
 }
 
 tasks.test {
