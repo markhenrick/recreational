16a19e60e94d0c54ce7cb446458ed4340ea0fd9f
Mark Henrick <2178745+markhenrick@users.noreply.github.com>
2024-12-03 11:22:54 +0000

    2024d3p2 Kotlin

--- kotlin/src/main/kotlin/site/markhenrick/recreational/adventofcode/y2024/Day03.kt
+++ kotlin/src/main/kotlin/site/markhenrick/recreational/adventofcode/y2024/Day03.kt
@@ -1,12 +1,41 @@
 package site.markhenrick.recreational.adventofcode.y2024
 
 object Day03 {
-    private val pattern = Regex("mul\\((\\d+),(\\d+)\\)")
+    private val mulPattern = Regex("mul\\((\\d+),(\\d+)\\)")
+    private val doPattern = Regex("do\\(\\)")
+    private val dontPattern = Regex("don't\\(\\)")
 
-    fun solvePart1(input: String) = pattern.findAll(input)
-        .map {
-            assert(it.groups.size == 2 + 1)
-            it.groups[1]!!.value.toInt() * it.groups[2]!!.value.toInt()
-        }
+    fun solvePart1(input: String) = mulPattern.findAll(input)
+        .map { evalMul(it) }
         .sum()
+
+    // Decided a state machine was easier to read for this part
+    fun solvePart2(input: String): Int {
+        var enabled = true
+        var offset = 0
+        var sum = 0
+        while (offset < input.length) {
+            if (enabled) {
+                val nextMul = mulPattern.find(input, offset) ?: break
+                val nextDont = dontPattern.find(input, offset)
+                if (nextDont != null && nextDont.range.last < nextMul.range.last) {
+                    enabled = false
+                    offset = nextDont.range.last
+                } else {
+                    sum += evalMul(nextMul)
+                    offset = nextMul.range.last
+                }
+            } else {
+                val nextDo = doPattern.find(input, offset) ?: break
+                enabled = true
+                offset = nextDo.range.last
+            }
+        }
+        return sum
+    }
+
+    private fun evalMul(mul: MatchResult): Int {
+        assert(mul.groups.size == 2 + 1)
+        return mul.groups[1]!!.value.toInt() * mul.groups[2]!!.value.toInt()
+    }
 }
--- kotlin/src/test/kotlin/site/markhenrick/recreational/adventofcode/y2024/Day03Test.kt
+++ kotlin/src/test/kotlin/site/markhenrick/recreational/adventofcode/y2024/Day03Test.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import site.markhenrick.recreational.adventofcode.y2024.Day03.solvePart1
+import site.markhenrick.recreational.adventofcode.y2024.Day03.solvePart2
 import site.markhenrick.recreational.getChallengeInput
 import java.util.stream.Stream
 
@@ -15,14 +16,27 @@ class Day03Test {
         assertThat(solvePart1(input)).isEqualTo(expected)
     }
 
+    @ParameterizedTest
+    @MethodSource
+    fun part2(input: String, expected: Int) {
+        assertThat(solvePart2(input)).isEqualTo(expected)
+    }
+
     companion object {
-        private val sampleInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+        private val sampleInput1 = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+        private val sampleInput2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
         private val challengeInput = getChallengeInput(2024, 3)
 
         @JvmStatic
         fun part1(): Stream<Arguments> = Stream.of(
-            Arguments.of(sampleInput, 161),
+            Arguments.of(sampleInput1, 161),
             Arguments.of(challengeInput, 178794710),
+        )
+
+        @JvmStatic
+        fun part2(): Stream<Arguments> = Stream.of(
+            Arguments.of(sampleInput2, 48),
+            Arguments.of(challengeInput, 76729637),
         )
     }
 }
