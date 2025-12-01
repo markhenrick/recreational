d0f4d67d1caf5973e013c670c85f3284889cb339
Mark Henrick <2178745+markhenrick@users.noreply.github.com>
2025-12-01 12:27:54 +0000

    2025d1p2 Kotlin

--- kotlin/src/main/kotlin/site/markhenrick/recreational/adventofcode/y2025/Day01.kt
+++ kotlin/src/main/kotlin/site/markhenrick/recreational/adventofcode/y2025/Day01.kt
@@ -1,17 +1,40 @@
 package site.markhenrick.recreational.adventofcode.y2025
 
+import kotlin.math.max
+import kotlin.math.min
+
 private const val MODULO = 100
 private const val INITIAL = 50
 
 object Day01 {
-    fun solvePart1(input: String): Int = input.lineSequence()
-        .map {
-            assert(it[0] == 'L' || it[0] == 'R')
-            val sign = if (it[0] == 'L') -1 else 1
-            val value = it.substring(1).toInt()
-            return@map sign * value
-        }
-        .runningFold(INITIAL) { a, b -> Math.floorMod(a + b, MODULO) }
-        .filter { it == 0 }
+    fun solvePart1(input: String): Int = getSequence(input)
+        .filter(Int::isZeroModulo)
         .count()
+
+    fun solvePart2(input: String): Int = getSequence(input)
+        .windowed(2, 1) { (a, b) -> zeroCrosses(a, b) }
+        .sum()
+
+    private fun parseInstruction(input: String): Int {
+        assert(input[0] == 'L' || input[0] == 'R')
+        val sign = if (input[0] == 'L') -1 else 1
+        val value = input.substring(1).toInt()
+        return sign * value
+    }
+
+    private fun getSequence(input: String): Sequence<Int> = input.lineSequence()
+        .map(::parseInstruction)
+        .runningFold(INITIAL) { a, b -> a + b }
+
+    // TODO replace this with a less-stupid version. I tried several times, but always failed to satisfy every edge case
+    internal fun zeroCrosses(start: Int, end: Int): Int {
+        var count = 0
+        for (i in min(start, end)..max(start, end)) {
+            if (i.isZeroModulo()) count++
+        }
+        if (start.isZeroModulo()) count--
+        return count
+    }
 }
+
+private fun Int.isZeroModulo(): Boolean = Math.floorMod(this, MODULO) == 0
--- kotlin/src/test/kotlin/site/markhenrick/recreational/adventofcode/y2025/Day01Test.kt
+++ kotlin/src/test/kotlin/site/markhenrick/recreational/adventofcode/y2025/Day01Test.kt
@@ -1,11 +1,10 @@
 package site.markhenrick.recreational.adventofcode.y2025
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.MethodSource
-import site.markhenrick.recreational.adventofcode.y2024.Day05Test
 import site.markhenrick.recreational.getChallengeInput
 import java.util.stream.Stream
 
@@ -14,6 +13,34 @@ class Day01Test {
     @MethodSource
     fun part1(input: String, expected: Int) {
         assertThat(Day01.solvePart1(input)).isEqualTo(expected)
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    fun part2(input: String, expected: Int) {
+        assertThat(Day01.solvePart2(input)).isEqualTo(expected)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "0, 99, 0",
+        "50, 150, 1",
+        "50, 250, 2",
+        "50, 1050, 10",
+        "50, -18, 1", // The dial starts by pointing at 50.; The dial is rotated L68 to point at 82; during this rotation, it points at **0 once**.
+        "-18, -48, 0", // The dial is rotated L30 to point at 52.
+        "-48, 0, 1", // The dial is rotated R48 to point at **0**.
+        "0, -5, 0", // The dial is rotated L5 to point at 95.
+        "-5, 55, 1", // The dial is rotated R60 to point at 55; during this rotation, it points at **0 once**.
+        "55, 0, 1",  // The dial is rotated L55 to point at **0**.
+        "0, -1, 0", // The dial is rotated L1 to point at 99.
+        "-1, -100, 1", // The dial is rotated L99 to point at **0**.
+        "-100, -86, 0", // The dial is rotated R14 to point at 14.
+        "-86, -168, 1", // The dial is rotated L82 to point at 32; during this rotation, it points at **0 once**.
+
+    )
+    fun zeroCrosses(start: Int, end: Int, expected: Int) {
+        assertThat(Day01.zeroCrosses(start, end)).isEqualTo(expected)
     }
 
     companion object {
@@ -35,6 +62,12 @@ class Day01Test {
         fun part1(): Stream<Arguments> = Stream.of(
             Arguments.of(sampleInput, 3),
             Arguments.of(challengeInput, 1059)
+        )
+
+        @JvmStatic
+        fun part2(): Stream<Arguments> = Stream.of(
+            Arguments.of(sampleInput, 6),
+            Arguments.of(challengeInput, 6305)
         )
     }
 }
