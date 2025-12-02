e1da18726c12ee783f068039b250e001da58e1c5
Mark Henrick <2178745+markhenrick@users.noreply.github.com>
2025-12-02 19:42:26 +0000

    2025d2p1 Kotlin

--- kotlin/src/main/kotlin/site/markhenrick/recreational/common/NumUtil.kt
+++ kotlin/src/main/kotlin/site/markhenrick/recreational/common/NumUtil.kt
@@ -1,15 +1,17 @@
 package site.markhenrick.recreational.common
 
-import kotlin.math.ceil
-import kotlin.math.log10
-
 infix fun Int.wrapMod(divisor: Int): Int = ((this % divisor) + divisor) % divisor
 
 // i.e. ceil(log10(x+1)), but with integer operations only
 fun base10DigitCount(x: Long): Int {
-    // TODO integer only
-    require(x >= 0)
-    return ceil(log10((x + 1).toDouble())).toInt()
+    // Let's just do it the simple way. No evil bit hacking. Yet
+    var count = 0
+    var current = x
+    while (current > 0) {
+        current /= 10
+        count++
+    }
+    return count
 }
 
 fun intPow(base: Int, exponent: Int): Long {
